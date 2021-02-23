225b981b3d7a8fa795b704daee411707d9a17762
Addisalem <addisalemdes@gmail.com>
2021-02-22 21:29:47 -0600

    d1w1assignment

--- first_program.js
+++ first_program.js
@@ -1,1 +1,11 @@
 console.log("helllo world")
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Pete"},
+    {id: 3, name: "Mary"}
+  ];
+  
+  // returns array of the first two users
+  let filteredUsers = users.filter(u => u.id < 3);
+  
+  console.log(filteredUsers.length); // 2
